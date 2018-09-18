da4fc0453f1922b6abe71a31449422cc5c9a997b
Jeferson Tenorio <jefersonsv@gmail.com>
2018-09-18 16:39:10 -0300

    fix broken pieces

---
+++ src/Parsers/Converter/SlugmeTypeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Parsers.Converter
+{
+    public class SlugmeTypeConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context,
+           System.Globalization.CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                return new Slugme(value as string);
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
---
+++ src/Parsers/Slugme.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using Parsers.Converter;
+
+namespace Parsers
+{
+    [System.Diagnostics.DebuggerDisplay("{Slug}")]
+    [TypeConverter(typeof(SlugmeTypeConverter))]
+    public class Slugme
+    {
+        public string Slug { get; private set; }
+        public string Text { get; private set; }
+
+        public string Encoded { get; private set; }
+
+        public Slugme(string text)
+        {
+            Text = text;
+            Slug = Underscore(RemoveDiacritics(text)).Replace("_", "-");
+        }
+
+        public static implicit operator Slugme(string text)
+        {
+            return new Slugme(text);
+        }
+
+        public override string ToString()
+        {
+            return Slug;
+        }
+
+        string Underscore(string input)
+        {
+            return Regex.Replace(
+                Regex.Replace(
+                    Regex.Replace(input, @"([\p{Lu}]+)([\p{Lu}][\p{Ll}])", "$1_$2"), @"([\p{Ll}\d])([\p{Lu}])", "$1_$2"), @"[-\s]", "_").ToLower();
+        }
+
+        static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
